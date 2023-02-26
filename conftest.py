81c8ea6ee6311ccba11199b8df94019b1364768f
viacheslavzorin <zorin-viacheslav@rambler.ru>
2023-02-26 21:29:08 +0300

    add  instantiate_from_csv, long_name, is_integer

--- conftest.py
+++ conftest.py
@@ -4,5 +4,5 @@ from item import Item
 
 @pytest.fixture
 def test_data():
-     item3 = Item("Смартфон", 20, 10000)
+     item3 = Item("Смартфон", 5, 20000)
      return item3
--- item.py
+++ item.py
@@ -1,24 +1,65 @@
-
-
+import csv
 class Item:
     instances = []
     num_of_item = 0
     pay_rate = 0.85
 
-    def __init__(self, product, quanity, price):
-        self.product = product
+    def __init__(self, product: str, quanity: int, price: int):
+        self.__product = product
         self.quanity = quanity
         self.price = price
         self.instances.append(self)
-
         Item.num_of_item += 1
 
     def calculate_total_price(self):
         return self.quanity * self.price
 
+    @property
     def apply_discount(self):
         self.price = self.price * self.pay_rate
         return self.price
+
+    #@property
+    #def product(self):
+    #    return self.product
+    #@product.setter
+    #def name(self, value: str):
+    #    if len(value) <= 10:
+    #        self.product = value
+    #    else:
+     #       print("Exception: Длина наименования товара превышает 10 символов")
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        results = []
+        with open('items.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                __product = row['name']
+                price = int(row['price'])
+                quanity = int(row['quantity'])
+                results.append(cls(__product, quanity, price))
+        return results
+    @staticmethod
+    def is_integer(number):
+        if number %1 ==0:
+            return True
+        else:
+            return False
+
+
+    @property
+    def long_name(self):
+        return self.__product
+    @long_name.setter
+    def long_name(self, name):
+        if len(name) <= 10:
+            self.__product = name
+        else:
+            self.__product = print("Exception: Длина наименования товара превышает 10 символов")
+            #raise Exception('Длина наименования товара превышает 10 символов')
+
+
 
 
 
